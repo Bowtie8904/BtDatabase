17229ae9c772f29731428187da6b794711028954
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-08 22:21:51 +0100

    Adjusted comment.

--- src/bt/db/listener/anot/ListenOn.java
+++ src/bt/db/listener/anot/ListenOn.java
@@ -6,11 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import bt.db.listener.DatabaseListener;
+
 /**
  * Defines the database table to listen on.
  * 
  * <p>
- * This annotation needs to be added to the receive methods of the {@link Listener} implementation.
+ * This annotation needs to be added to the receive methods of the {@link DatabaseListener} implementation.
  * </p>
  * 
  * <p>
