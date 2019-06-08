8952047b21718f2494a8e603c9bf62fb436933ec
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-06-08 22:19:21 +0200

    Changed to represent all join types.

--- src/bt/db/statement/clause/join/JoinClause.java
+++ src/bt/db/statement/clause/join/JoinClause.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import bt.db.statement.clause.ConditionalClause;
-import bt.db.statement.clause.JoinConditionalClause;
 import bt.db.statement.impl.SelectStatement;
 
 /**
@@ -12,16 +11,30 @@ import bt.db.statement.impl.SelectStatement;
  * 
  * @author &#8904
  */
-public abstract class JoinClause<T extends JoinClause>
+public class JoinClause
 {
+    protected static final String INNER = "INNER";
+    protected static final String RIGHT = "RIGHT";
+    protected static final String LEFT = "LEFT";
+    protected static final String NATURAL = "NATURAL";
+
     /** The statement which created this instance. */
     protected SelectStatement statement;
 
     /** The involved tables. table1 = left side, table2 = right side. */
     protected String table1, table2;
 
-    /** Contains all {@link JoinConditionalClause}s which were used to create this join. */
+    /** Contains all {@link JoinConditionalClause}s which were used in this joins ON clause. */
     protected List<JoinConditionalClause> conditionalClauses;
+
+    /** The type of this join. Default = INNER. */
+    protected String joinType = INNER;
+
+    /** The columns used to join the tables with the USING keyword. */
+    private String[] joinedColumns;
+
+    /** Indicates whether this join uses the USING or the ON syntax. true = using, false = on. */
+    private boolean using;
 
     /**
      * Creates a new instance and initializes fields.
@@ -103,11 +116,102 @@ public abstract class JoinClause<T extends JoinClause>
     }
 
     /**
+     * Defines the columns which should be used to join.
+     * 
+     * <p>
+     * Making use of this method means that no complex ON syntax can be used.
+     * </p>
+     * 
+     * @param columns
+     * @return
+     */
+    public SelectStatement using(String... columns)
+    {
+        this.joinedColumns = columns;
+        this.using = true;
+        return this.statement;
+    }
+
+    /**
+     * Defines that this join should be treated as a natural join clause, meaning no specific columns are set and the
+     * tables are joined around columns with the same name instead.
+     * 
+     * @return
+     */
+    public SelectStatement natural()
+    {
+        this.joinType = NATURAL;
+        return this.statement;
+    }
+
+    /**
+     * Defines that this join should be treated as a right join clause.
+     * 
+     * @return
+     */
+    public JoinClause right()
+    {
+        this.joinType = RIGHT;
+        return this;
+    }
+
+    /**
+     * Defines that this join should be treated as a left join clause.
+     * 
+     * @return
+     */
+    public JoinClause left()
+    {
+        this.joinType = LEFT;
+        return this;
+    }
+
+    /**
+     * Returns the String representing this join clause.
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return toString(false);
+    }
+
+    /**
      * Returns the String representing this join clause.
      * 
      * @param prepared
      *            Indicates whether underlying conditionals from the ON syntax should be treated as prepared statements
      *            or inserted as plain text. true = use prepared statement, false = insert plain.
      */
-    public abstract String toString(boolean prepared);
+    public String toString(boolean prepared)
+    {
+        String sql = this.joinType + " JOIN " + this.table2;
+
+        if (!this.joinType.equals(NATURAL))
+        {
+            if (this.using)
+            {
+                sql += " USING (";
+
+                for (String column : this.joinedColumns)
+                {
+                    sql += column + ", ";
+                }
+
+                sql = sql.substring(0, sql.length() - 2);
+
+                sql += ")";
+            }
+            else
+            {
+                for (JoinConditionalClause clause : this.conditionalClauses)
+                {
+                    sql += " " + clause.toString(prepared);
+                }
+            }
+        }
+
+        return sql;
+    }
 }
